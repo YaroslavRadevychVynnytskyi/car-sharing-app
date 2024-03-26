24d9910533245f6cc107f1dcc8dd9989ae8e6202
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-03-26 18:38:05 +0200

    Initial commit

---
+++ src/main/java/application/carsharingapp/CarSharingAppApplication.java
@@ -0,0 +1,12 @@
+package application.carsharingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingAppApplication.class, args);
+    }
+}
